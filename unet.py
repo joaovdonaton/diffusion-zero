d6d836251781ba15740df669eead7a31a885c23e
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-05-28 21:18:28 -0300

    unet implementation

--- unet.py
+++ unet.py
@@ -104,7 +104,7 @@ class Encoder(nn.Module):
             [ResidualLayer(c_in, t_embed_dim, y_embed_dim) for _ in range(num_residual_layers)]
         )
 
-        self.downsample = nn.Conv2d(c_in, c_out, kernel_size=3, stride=2, padding=1)
+        self.downsample = nn.Conv2d(c_in, c_out, kernel_size=3, stride=2, padding=1) # 2x downsample
     
     def forward(self, x, temb, yemb):
         """
@@ -121,16 +121,124 @@ class Encoder(nn.Module):
         return x
 
 
-class UNet(nn.Module):
-    def __init__(self):
+# No down/up sampling, just residuals
+class Midcoder(nn.Module):
+    def __init__(self, c_in, num_residual_layers, t_embed_dim, y_embed_dim):
+        """
+        Args: All integers
+        """
         super().__init__()
+        self.res_blocks = nn.ModuleList(
+            [ResidualLayer(c_in, t_embed_dim, y_embed_dim) for _ in range(num_residual_layers)]
+        )
+    
+    def forward(self, x, temb, yemb):
+        """
+        Args:
+        - x: (bs, c_in, h, w)
+        - t_embed: (bs, t_embed_dim)
+        - y_embed: (bs, y_embed_dim)
+        """
+        for b in self.res_blocks:
+            x = b.forward(x, temb, yemb)
 
+        return x
+
+
+# upsampling convolutions, followed by N residual layers
+class Decoder(nn.Module):
+    def __init__(self, c_in, c_out, num_residual_layers, t_embed_dim, y_embed_dim):
+        """
+        Args: All integers
+        """
+        super().__init__()
+        self.res_blocks = nn.ModuleList(
+            [ResidualLayer(c_out, t_embed_dim, y_embed_dim) for _ in range(num_residual_layers)]
+        )
+
+        self.upsample = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear'), nn.Conv2d(c_in, c_out, kernel_size=3, padding=1))
+    
+    def forward(self, x, temb, yemb):
+        """
+        Args:
+        - x: (bs, c_in, h, w)
+        - t_embed: (bs, t_embed_dim)
+        - y_embed: (bs, y_embed_dim)
+        """
+        x = self.upsample(x)
+
+        for b in self.res_blocks:
+            x = b.forward(x, temb, yemb)
+
+        return x
+
+class UNet(nn.Module):
+    def __init__(self, channels, num_residual_layers, t_embed_dim, y_embed_dim):
+        """
+        Args:
+        - channels: list
+        - others: integers
+        """
+        super().__init__()
+        
+        self.init_conv = nn.Sequential(nn.Conv2d(1, channels[0], kernel_size=3, padding=1), nn.BatchNorm2d(channels[0]), nn.SiLU())
+
+        self.time_embedder = TimeEmbedder(t_embed_dim)
+
+        ## CIFAR-10 has 10 classes, +1 for null class for classifier-free guidance
+        # nn.Embedding is a learned lookup table, more efficient than having a linear layer because we can just lookup embeddings instead of calculating every time
+        # also, encodes semantic structure in the y_embed_dim vector space as opposed to one hot encoding
+        self.y_embedder = nn.Embedding(num_embeddings = 11, embedding_dim = y_embed_dim)
+    
+        encoders = []
+        decoders = []
+        for (curr_c, next_c) in zip(channels[:-1], channels[1:]):
+            encoders.append(Encoder(curr_c, next_c, num_residual_layers, t_embed_dim, y_embed_dim))
+            decoders.append(Decoder(next_c, curr_c, num_residual_layers, t_embed_dim, y_embed_dim))
+        self.encoders = nn.ModuleList(encoders)
+        self.decoders = nn.ModuleList(reversed(decoders))
+
+        self.midcoder = Midcoder(channels[-1], num_residual_layers, t_embed_dim, y_embed_dim)
+
+        self.final_conv = nn.Conv2d(channels[0], 1, kernel_size=3, padding=1)
+
+    def forward(self, x, t, y):
+        """
+        Args:
+        - x: (bs, 1, h, w)
+        - t: (bs, 1, 1, 1)
+        - y: (bs,)
+        Returns:
+        - u_t^theta(x|y): (bs, 1, h, w)
+        """
+
+        temb = self.time_embedder.forward(t.squeeze(-1).squeeze(-1))
+        yemb = self.y_embedder(y)
+
+        x = self.init_conv(x)
+
+        residuals = []
+
+        for enc in self.encoders:
+            x = enc.forward(x, temb, yemb)
+            residuals.append(x.clone())
+
+        x = self.midcoder.forward(x, temb, yemb)
+
+        for dec in self.decoders:
+            res = residuals.pop()
+            x = x + res
+            x = dec(x, temb, yemb)
+
+        x = self.final_conv(x)
+
+        return x
 
 if __name__ == "__main__":
     # temb = TimeEmbedder(16)
     # print(temb.forward(torch.randn(4, 1)))
     
-    x = torch.randn(1, 4, 10, 10)
+    x = torch.randn(1, 1, 32, 32)
     temb = TimeEmbedder(32)
     yemb = torch.randn(1, 10)
 
@@ -143,3 +251,18 @@ if __name__ == "__main__":
     # y = enc.forward(x, temb.forward(torch.randn(1,1)), yemb)
     # print(y)
     # print(y.shape)
+    
+    # mid = Midcoder(4, 3, 32, 10)
+    # y = mid.forward(x, temb.forward(torch.randn(1,1)), yemb)
+    # print(y)
+    # print(y.shape)
+    
+    # dec = Decoder(4, 2, 2, 32, 10)
+    # y = dec.forward(x, temb.forward(torch.randn(1,1)), yemb)
+    # print(y)
+    # print(y.shape)
+
+    unet = UNet([2, 4, 8], 2, 32, 10)
+    y = unet(x, torch.randn(1, 1, 1, 1), torch.randint(11, (1,)))
+    print(y)
+    print(y.shape)
